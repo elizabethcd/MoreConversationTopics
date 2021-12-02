bb5e2539e7336e9e8966fb2c7ae9085b7d078154
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2021-12-01 22:41:44 -0500

    Adding more console commands

--- MoreConversationTopics/ModEntry.cs
+++ MoreConversationTopics/ModEntry.cs
@@ -43,7 +43,7 @@ namespace MoreConversationTopics
             WorldChangePatcher.Apply(harmony);
 
             // Adds a command to check current active conversation topics
-            helper.ConsoleCommands.Add("current_conversation_topics", "Dumps currently active dialogue events", (str, strs) =>
+            helper.ConsoleCommands.Add("current_conversation_topics", "Returns a list of the current active dialogue events", (str, strs) =>
             {
                 if (!Context.IsWorldReady)
                     return;
@@ -59,6 +59,13 @@ namespace MoreConversationTopics
 
             // Adds a command to see if player has a given mail flag
             helper.ConsoleCommands.Add("player_hasmailflag", "Checks if the player has a mail flag.\n\nUsage: player_hasmailflag <flagName>\n- flagName: the possible mail flag name.", this.HasMailFlag);
+
+            // Adds a command to add a conversation topic
+            helper.ConsoleCommands.Add("add_conversation_topic", "Adds the specified conversation topic with duration of 1 day.\n\nUsage: add_conversation_topic <flagName>\n- flagName: the conversation topic to add.", this.AddConversationTopic);
+
+            // Adds a command to remove a conversation topic
+            helper.ConsoleCommands.Add("remove_conversation_topic", "Removes the specified conversation topic.\n\nUsage: remove_conversation_topic <flagName>\n- flagName: the conversation topic to remove.", this.RemoveConversationTopic);
+
         }
 
         // Helper function to check if a string is on the list of repeatable CTs added by this mod
@@ -77,6 +84,9 @@ namespace MoreConversationTopics
         // Has Mail Flag function for console command
         private void HasMailFlag(string command, string[] args)
         {
+            if (!Context.IsWorldReady)
+                return;
+
             try
             {
                 if (Game1.player.mailReceived.Contains(args[0]))
@@ -87,6 +97,40 @@ namespace MoreConversationTopics
                 {
                     this.Monitor.Log($"No, you don't have this mail flag", LogLevel.Debug);
                 }
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Bad or missing argument with exception: {ex}", LogLevel.Error);
+            }
+        }
+
+        // Add conversation topic for console command
+        private void AddConversationTopic(string command, string[] args)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            try
+            {
+                Game1.player.activeDialogueEvents.Add(args[0], 1);
+                this.Monitor.Log($"Added conversation topic", LogLevel.Debug);
+            }
+            catch (Exception ex)
+            {
+                Monitor.Log($"Bad or missing argument with exception: {ex}", LogLevel.Error);
+            }
+        }
+
+        // Remove conversation topic for console command
+        private void RemoveConversationTopic(string command, string[] args)
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            try
+            {
+                Game1.player.activeDialogueEvents.Remove(args[0]);
+                this.Monitor.Log($"Removed conversation topic", LogLevel.Debug);
             }
             catch (Exception ex)
             {
